dc907ac1f49a8890fcb822a52f2a914243e1190b
SSE4 <tomskside@gmail.com>
2018-02-18 10:37:38 +0700

    Revert "- try to resolve arch_build"
    This reverts commit 4bc79240e6e508ebad24ca533e17bf617d3981a9.

--- build.py
+++ build.py
@@ -11,19 +11,12 @@ def add_required_installers(build):
     installers = ['ninja_installer/1.8.2@bincrafters/stable']
     build.build_requires.update({"*" : installers})
     return build
-
+    
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-    filtered_builds = []
-
+    
     builder.items = add_build_requires(builder.items)
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        settings["arch_build"] = settings["arch"]
-        filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
-    builder.run()
-
 
     builder.run()
-
+    
