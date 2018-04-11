001874dda7ddbcd1de0c537967861d26645484d6
Uilian Ries <uilianries@gmail.com>
2018-04-10 21:31:13 -0300

    Skip x86 build on Macos
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -11,12 +11,11 @@ def add_required_installers(build):
     installers = ['ninja_installer/1.8.2@bincrafters/stable']
     build.build_requires.update({"*" : installers})
     return build
-    
+
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-    
+
     builder.items = add_build_requires(builder.items)
 
     builder.run()
-    
