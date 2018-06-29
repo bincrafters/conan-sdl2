c4f3b799b2f31da4f5dd268d6205b46c49c50fe5
SSE4 <tomskside@gmail.com>
2018-06-29 21:43:43 +0700

    - build with custom options for madebr
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -16,6 +16,16 @@ if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
 
+    if tools.os_info.is_linux and os.getenv("CONAN_GCC_VERSIONS") == 8:
+        if not os.getenv("CONAN_BUILD_POLICY"):
+            os.environ["CONAN_BUILD_POLICY"] = "missing"
+        for shared_option in [False, True]:
+            custom_options = {"sdl2:esd": False, "sdl2:wayland": True, "sdl2:x11": True, 'sdl2:shared': shared_option}
+            builder.add({'arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+            builder.add({'arch': 'x86', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+            builder.add({'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+            builder.add({'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+
     builder.items = add_build_requires(builder.items)
 
     builder.run()
