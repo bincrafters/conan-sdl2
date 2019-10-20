409c9fdf5f6c871d7080e50832f4aa7b04324c71
Croydon <git@cr0ydon.com>
2019-10-20 18:32:48 +0200

    Remove x86 custom builds

--- build.py
+++ build.py
@@ -20,8 +20,6 @@ if __name__ == "__main__":
     if tools.os_info.is_linux and os.getenv("CONAN_GCC_VERSIONS") == 8:
         for shared_option in [False, True]:
             custom_options = {"sdl2:esd": False, "sdl2:wayland": True, "sdl2:x11": True, 'sdl2:shared': shared_option}
-            builder.add({'arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
-            builder.add({'arch': 'x86', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
             builder.add({'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
             builder.add({'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
 
