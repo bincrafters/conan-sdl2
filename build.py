3b69ee876efd019d1e48c5bfbb04ca55ae79a727
Croydon <git@cr0ydon.com>
2020-03-07 11:01:26 +0100

    build.py: Fix syntax

--- build.py
+++ build.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     gcc_version = os.getenv("CONAN_GCC_VERSIONS", None)
     clang_version = os.getenv("CONAN_CLANG_VERSIONS", None)
     compiler_version = gcc_version if gcc_version else clang_version
-    
-    if tools.os_info.is_linux and (gcc_version is in (8,9,10)  or clang_version is in (10,)):
+
+    if tools.os_info.is_linux and (gcc_version in (8,9,10) or clang_version in (10,)):
         for shared_option in [False, True]:
             custom_options = {"sdl2:esd": False, "sdl2:wayland": True, "sdl2:x11": True, 'sdl2:shared': shared_option}
             builder.add({'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc',
