0e662047682a56a0839bfcfe6aa9763b6befe9f7
Croydon <git@cr0ydon.com>
2020-03-07 14:19:26 +0100

    Format

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,14 +12,14 @@ class TestPackageConan(ConanFile):
     def build_cmake(self):
         cmake = CMake(self)
         if self.settings.os == "Linux":
-            cmake.definitions['WITH_X11'] = self.options['sdl2'].x11
-            cmake.definitions['WITH_ALSA'] = self.options['sdl2'].alsa
-            cmake.definitions['WITH_PULSE'] = self.options['sdl2'].pulse
-            cmake.definitions['WITH_ESD'] = self.options['sdl2'].esd
-            cmake.definitions['WITH_ARTS'] = self.options['sdl2'].arts
-            cmake.definitions['WITH_DIRECTFB'] = self.options['sdl2'].directfb
+            cmake.definitions["WITH_X11"] = self.options["sdl2"].x11
+            cmake.definitions["WITH_ALSA"] = self.options["sdl2"].alsa
+            cmake.definitions["WITH_PULSE"] = self.options["sdl2"].pulse
+            cmake.definitions["WITH_ESD"] = self.options["sdl2"].esd
+            cmake.definitions["WITH_ARTS"] = self.options["sdl2"].arts
+            cmake.definitions["WITH_DIRECTFB"] = self.options["sdl2"].directfb
         if self.settings.os == "Windows":
-            cmake.definitions['WITH_DIRECTX'] = self.options['sdl2'].directx
+            cmake.definitions["WITH_DIRECTX"] = self.options["sdl2"].directx
         cmake.configure()
         cmake.build()
 
